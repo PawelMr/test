07987834fa90dd20eb925b6cbf39545cc93f09ee
HOMEPC <pawel.mr@mail.ru>
2020-02-19 20:20:28 +0300

    new

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require('test.php');
+//var_dump(substr(12313, 0, 0));
+$per = 123.12;
+//var_dump(is_double($per));
+//var_dump(stripos('123', '1'));
+$food = [
+    ];
+var_dump(count($food));
